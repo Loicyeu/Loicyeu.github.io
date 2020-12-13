6955f6190fe384b05c479c4ed057b8a0d99f1af1
Loicyeu <loic.henry2001@gmail.com>
2020-12-13 23:09:10 +0100

    set retractable jumbotron

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,23 @@
-$(window).scroll(function () {
-    if($(this).scrollTop()>1)
-        console.log("top");
-    else
-        console.log("not top");
-});
+const jumbostronHeight = document.getElementsByClassName("jumbotron")[0].offsetHeight;
+const profilePictureHeight = document.getElementById("profilePicture").offsetHeight;
+
+
+$(function () {
+
+    $(window).on("scroll", function () {
+        let scrollPos = $(this).scrollTop();
+        let jumbotron = $(".jumbotron");
+
+        if(profilePictureHeight-scrollPos>=100) {
+            $("#profilePicture").css("height", profilePictureHeight - scrollPos);
+            jumbotron.css("height", jumbostronHeight - scrollPos);
+            jumbotron.css("padding", "4rem 2rem");
+            $(".presentationHide").show();
+        } else {
+            $("#profilePicture").css("height", 100);
+            jumbotron.css("padding", "2rem");
+            $(".presentationHide").hide();
+            jumbotron.css("height", "auto");
+        }
+    });
+})
