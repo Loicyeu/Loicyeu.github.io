53c412fc92fc14f3757b4add87e51e9ad0ec14cb
Loicyeu <loic.henry2001@gmail.com>
2021-01-05 19:56:15 +0100

    add dark mode v1

--- assets/script/script.js
+++ assets/script/script.js
@@ -26,12 +26,21 @@ $(function () {
             $("#skills").css("margin-top", jumbotron.outerHeight()+scrollPos);
     });
 
-    // $("#darkMode").on("click", function () {
-    //     console.log('test');
-    //     if($("#darkMode:checked").length===0) {
-    //         console.log("not selected");
-    //     }else {
-    //         console.log("selected");
-    //     }
-    // });
+    $("#darkMode").on("click", function () {
+        if($("#darkMode:checked").length===0) {
+            //SET TO LIGHT MODE
+            $(document.body).addClass("bg-white").removeClass("bg-dark");
+            $(".jumbotron").removeClass("bg-darker");
+            $(".bg-darker").addClass("bg-light").removeClass("bg-darker");
+            $("p, li, h1, h2, h3, h4, h5, h6, label").addClass("text-dark").removeClass("text-light");
+            $(".card").removeClass("bg-darkgrey");
+        }else {
+            //SET TO DARK MODE
+            $(document.body).addClass("bg-dark").removeClass("bg-white");
+            $(".jumbotron").addClass("bg-darker");
+            $(".bg-light").addClass("bg-darker").removeClass("bg-light");
+            $("p, li, h1, h2, h3, h4, h5, h6, label").addClass("text-light").removeClass("text-dark");
+            $(".card").addClass("bg-darkgrey")
+        }
+    });
 });
